5620087cac98b0bb4e612f3dbe2563e4faa9a08a
Ancelmo Rodríguez Parra <ancelmo.rodriguez@gmail.com>
2025-03-12 20:31:14 -0600

    desafío terminado

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # challenge-amigo-secreto_esp-main
+<h1>Descripción de su proyecto;</h1>
+Proyecto de prueba para sorteo de amigos
+<h1>funcionalidades;</h1>
+permite agregar amigos a una lista, mostrarlos y seleccionar uno de un sorteo
+<h1>Cómo pueden usarlo los usuarios;</h1>
+Navegador web
+<h1>Donde los usuarios pueden encontrar ayuda sobre su proyecto;</h1>
+En este README
+<h1>Autores del proyecto.</h1>
+ancelmo.rodriguez@gmail.com
--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+  if(nombre == ""){
+    alert("Por favor, inserte un nombre.");
+  }
+    else{
+        amigos.push(nombre);
+        document.getElementById('amigo').value = "";
+        document.getElementById('amigo').focus();
+        console.log(amigos);
+        actualizarLista();
+    }
+}
+
+function actualizarLista() {
+    var ul = document.getElementById("listaAmigos");
+    ul.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(amigos[i]));
+        ul.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length == 0) {
+        alert("No hay suficientes amigos para sortear.");
+    } else {
+        let amigoSorteado = amigos[(Math.floor(Math.random() * amigos.length))];
+        document.getElementById("resultado").innerHTML = amigoSorteado;
+    }
+}
